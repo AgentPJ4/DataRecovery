dec27628e17c126d5023c655e18ca35b975665f3
AgentPJ4 <agentpj4@gmail.com>
2025-03-25 16:11:16 +0100

    Figruing out colors for echoes.

--- Data-Recovery.sh
+++ Data-Recovery.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-echo "Welcome to Data Recovery!"
-echo "Please insert the name of the device you want to recover data from :"
+echo -e "\e[31mWarning: The provided device must be found under /dev/...\e[0m"
+echo -e "\e[33mWelcome to Data Recovery!\e[0m"
+echo -e "\e[32mPlease insert the name of the device you want to recover data from:\e[0m"
 read diskname
-
 if [[ -z $(echo "$diskname" | xargs) ]]; then
-	echo "Error: No device name entered. Please provide a valid disk name!"
-	exit 1
+    echo -e "\e[31mError: No device name entered. Please provide a valid disk name!\e[0m"
+    exit 1
 fi
-echo "Give path where you want to store the recovered data: "
-echo "Example : /home/Documents/Directory_for_Recovered_Data"
+echo -e "\e[32mGive path where you want to store the recovered data:\e[0m"
+echo -e "\e[32mExample: /home/Documents/Directory_for_Recovered_Data\e[0m"
 read writepath
 if [[ -z $(echo "$writepath" | xargs) ]]; then
-        echo "Error: A path must be provided in order to continue!"
-        exit 1
+    echo -e "\e[31mError: A path must be provided in order to continue!\e[0m"
+    exit 1
 fi
-
-echo "Proceeding..."
+echo -e "\e[37mProceeding...\e[0m"
 
