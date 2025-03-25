63e486245b5dccf1ea3ac606c9a397ddeb207854
AgentPJ4 <agentpj4@gmail.com>
2025-03-25 16:00:37 +0100

    Starting basic input testing logic.

--- Data-Recovery.sh
+++ Data-Recovery.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+echo "Welcome to Data Recovery!"
+echo "Please insert the name of the device you want to recover data from :"
+read diskname
 
-echo "Testing project beginn."
+if [[ -z $(echo "$diskname" | xargs) ]]; then
+	echo "Error: No device name entered. Please provide a valid disk name!"
+	exit 1
+fi
+echo "Give path where you want to store the recovered data: "
+echo "Example : /home/Documents/Directory_for_Recovered_Data"
+read writepath
+if [[ -z $(echo "$writepath" | xargs) ]]; then
+        echo "Error: A path must be provided in order to continue!"
+        exit 1
+fi
+
+echo "Proceeding..."
+
